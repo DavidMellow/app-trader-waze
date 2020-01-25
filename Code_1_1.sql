83a51c0dd4284ee6ada9ea60ff2c16d96347cb36
David Mellow <TheMellows@MacBook-Pro-4.local>
2020-01-25 10:49:24 -0600

    ignore

--- Code_1_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT a_store.name AS a_name, p_store.name AS p_name,
-	ROUND(AVG(a_store.rating),1)AS a_avg_rate,
-	ROUND(AVG(p_store.rating),1)AS p_avg_rate
-FROM app_store_apps AS a_store
-LEFT JOIN play_store_apps AS p_store
-ON a_store.name = p_store.name 
-GROUP BY a_store.name, p_store.name;
-
-SELECT a_avg_rate, p_avg_rate, a_lifespan, p_lifespan
--- Code_Common_APPs.sql
+++ Code_Common_APPs.sql
@@ -38,11 +38,7 @@ SELECT
 	ROUND(12*(a.rating/.5)+12,0) AS a_proj_mo_ls,
 	ROUND(12*(p.rating/.5)+12,0) AS p_proj_mo_ls,--projected lifespan of app (in months); round to nearest .5
 	(.5*5000)* ROUND(12*(a.rating/.5)+12,0) AS proj_ls_earn, --app trader's projected lifespan earnings
-	(SELECT 
-	 	CASE  
-	 		WHEN a.price <= 1.00 THEN  10000
-	 		ELSE 10000 * a.price 
-	   		END AS a_purch_price),--purchase price is $10,000 * app price, but app price <= $1, then purchase price is $10,000
+	10000 * a.price AS a_purch_price,--purchase price is $10,000 * app price, but app price <= $1, then purchase price is $10,000
 	1000*(1+.5*a.rating)*12 AS proj_ls_cost, --projected monthly maintenance cost is $1000 * lifespan in months
 	1000* (ROUND(12*(a.rating/.5)+12,0)) AS a_ls_proj_rev,--projected life span rev.from app store
 	primary_genre AS app_genre,
